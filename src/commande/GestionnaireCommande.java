bfaaed8066f4b7046162f0edbe2c93ea609f02f1
KathleenFrancis01 <kathleen-francis-mbo.kathleen-francis-mbo.1@ens.etsmtl.ca>
2022-03-23 16:26:15 -0400

    Modifier GestionnaireCommande

--- src/commande/GestionnaireCommande.java
+++ src/commande/GestionnaireCommande.java
@@ -1,4 +1,20 @@
 package commande;
 
+import java.util.LinkedList;
+import commande.Commande;
+
 public class GestionnaireCommande {
+
+    private LinkedList<Commande> listeCmdPersp1;
+    private LinkedList<Commande> listeCmdPersp2;
+    private static GestionnaireCommande instance;
+
+    private void gestionnaireCommande() {
+        instance = new GestionnaireCommande();
+    }
+
+    public static GestionnaireCommande getInstance() {
+        return instance;
+    }
+
 }
