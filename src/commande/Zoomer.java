9b817f642e53c6dff18f8f6288edfd527cccca96
KathleenFrancis01 <kathleen-francis-mbo.kathleen-francis-mbo.1@ens.etsmtl.ca>
2022-03-23 16:12:42 -0400

    classes modifiées : Zoom, Dezoomer et Translater

--- src/commande/Zoomer.java
+++ src/commande/Zoomer.java
@@ -1,4 +1,24 @@
 package commande;
 
+import modele.Perspective;
+
 public class Zoomer {
+
+    private Perspective modelePerspective;
+
+    public void commandeZoomer(Perspective modelePerspective) {
+        this.modelePerspective = modelePerspective;
+    }
+
+    @Override
+    public boolean execute() {
+        modelePerspective.zoomer();
+        return true;
+    }
+
+    public boolean defaire() {
+        modelePerspective.dezoomer();
+        return true;
+    }
+
 }
