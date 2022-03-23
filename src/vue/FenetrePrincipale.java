fa3e0939a28d386c4b6ef7b3c0d20adc152a0fcd
Julian Maldonado <julian.maldonado@arvisiongames.com>
2022-03-23 15:13:38 -0400

    Modif ProgrammePrincipale et FenetrePrincipale

--- src/vue/FenetrePrincipale.java
+++ src/vue/FenetrePrincipale.java
@@ -1,4 +1,43 @@
 package vue;
 
-public class FenetrePrincipale {
+import javax.swing.JFrame;
+import java.awt.Dimension;
+
+public class FenetrePrincipale extends JFrame implements Runnable{
+
+    private static final String TITRE_FENETRE = "Laboratoire 1 : LOG121 - Simulation";
+    private static final Dimension DIMENSION = new Dimension(700, 700);
+
+    //private PanneauPrincipale panneauPrincipale;
+    private MenuFenetre menuFenetre;
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+
+    }
+
+    /**
+     *
+     */
+    private void initFenetre() {
+
+    }
+
+    /**
+     *
+     */
+    private void initContenu() {
+
+    }
 }
