398e46f8458c1a771fddd9015e8f221a6cd197f3
Kathleen <kfmbo01@gmail.com>
2022-04-02 20:17:42 -0400

    serialization image en cours

--- src/modele/ModelePrincipal.java
+++ src/modele/ModelePrincipal.java
@@ -1,11 +1,15 @@
 package modele;
 
 import observateur.Observable;
+
+import java.awt.image.RenderedImage;
 import java.io.*;
 import java.io.File;
 import java.util.LinkedList;
 
 import vue.VuePerspective;
+
+import javax.imageio.ImageIO;
 
 public class ModelePrincipal extends Observable {
 
@@ -13,6 +17,7 @@ public class ModelePrincipal extends Observable {
 
     private Perspective perspectiveFinale1;
     private Perspective perspectiveFinale2;
+    private LinkedList<Perspective> perspectives = new LinkedList<>();
 
     public void ouvrir(File pathImage) {
         //this.pathImage = pathImage.toString();
@@ -22,16 +27,28 @@ public class ModelePrincipal extends Observable {
 
     public void sauvegarder (VuePerspective vuePerspectiveFinale1, VuePerspective vuePerspectiveFinale2, String nameSavedFile, File pathSavedFile) {
         System.out.println("modeleprinc.java");
-        LinkedList<Perspective> perspectives = new LinkedList<>();
         try {
+            //transient vuePerspectiveFinale1.getPerspective().getImage();
             FileOutputStream fileOut = new FileOutputStream( pathSavedFile + nameSavedFile + ".ser");
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             perspectives.add(vuePerspectiveFinale1.getPerspective());
             perspectives.add(vuePerspectiveFinale2.getPerspective());
-            out.writeObject(perspectives);
+            //out.writeObject(perspectives);
+            out.defaultWriteObject();
+            //out.writeInt(images.size()); // how many images are serialized?
+            //for (BufferedImage eachImage : images) {
+                ImageIO.write((RenderedImage) vuePerspectiveFinale1.getPerspective().getImage(), "png", out); // png is lossless
+            //}
             out.close();
             fileOut.close();
             System.out.println("Serialized data is saved in " + pathSavedFile + nameSavedFile + ".ser");
+
+            //transient List<BufferedImage> images;
+            /*out.defaultWriteObject();
+            out.writeInt(images.size()); // how many images are serialized?
+            for (BufferedImage eachImage : images) {
+                ImageIO.write(eachImage, "png", out); // png is lossless
+            }*/
         } catch (IOException i) {
             i.printStackTrace();
         }
